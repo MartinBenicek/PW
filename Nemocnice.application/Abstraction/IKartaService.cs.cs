9c28edb5aff1c9c4adca6a65d91d30214a76ffaa
Václav Tomeček <vaclavtomecek1@gmail.com>
2024-12-16 19:18:29 +0100

    IOrdinace, IKarta

---
+++ Nemocnice.application/Abstraction/IKartaService.cs.cs
@@ -0,0 +1,16 @@
+﻿using Nemocnice.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemocnice.application.Abstraction
+{
+    public interface IKartaService
+    {
+        IList<Karta> Select();
+        void Create(Karta karta);
+        bool Delete(int id);
+    }
+}
---
+++ Nemocnice.application/Abstraction/IOrdinaceService.cs
@@ -0,0 +1,16 @@
+﻿using Nemocnice.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nemocnice.application.Abstraction
+{
+    public interface IOrdinaceService
+    {
+        IList<Ordinace> Select();
+        void Create(Ordinace ordinace);
+        bool Delete(int id);
+    }
+}
