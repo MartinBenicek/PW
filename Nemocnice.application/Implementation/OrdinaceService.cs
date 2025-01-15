cce1326a5be42add1fb1abcbd8a12cad25f437d4
MartinBenicek <martabenic@seznam.cz>
2025-01-15 19:13:27 +0100

    image src only

--- Nemocnice.application/Implementation/OrdinaceService.cs
+++ Nemocnice.application/Implementation/OrdinaceService.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Nemocnice.application.Abstraction;
+﻿using Nemocnice.application.Abstraction;
+using Nemocnice.application.ViewModels;
 using Nemocnice.domain.Entities;
 using Nemocnice.infrastructure.Database;
 
@@ -13,15 +8,86 @@ namespace Nemocnice.application.Implementation
    
     public class OrdinaceAppService : IOrdinaceService
     {
-        NemocniceDbContext _NemocniceDbContext;
-        public OrdinaceAppService(NemocniceDbContext NemocniceDbContext)
+        private readonly NemocniceDbContext _context;
+        private readonly IFileUploadService _fileUploadService;
+
+        public OrdinaceAppService(NemocniceDbContext context, IFileUploadService fileUploadService)
         {
-            _NemocniceDbContext = NemocniceDbContext;
+            _context = context;
+            _fileUploadService = fileUploadService;
         }
 
-        public IList<Ordinace> Select()
+        public List<OrdinaceViewModel> GetOrdinace()
         {
-            return _NemocniceDbContext.Ordinace.ToList();
+            return _context.Ordinace.Select(o => new OrdinaceViewModel
+            {
+                OrdinaceId = o.Id,
+                Budova = o.Budova,
+                Mistnost = o.Mistnost,
+                DoktorId = o.DoktorID,
+                ImageSrc = o.ImageSrc
+            }).ToList();
+        }
+
+        public OrdinaceViewModel GetOrdinaceById(int id)
+        {
+            return _context.Ordinace.Where(o => o.Id == id).Select(o => new OrdinaceViewModel
+            {
+                OrdinaceId = o.Id,
+                Budova = o.Budova,
+                Mistnost = o.Mistnost,
+                DoktorId = o.DoktorID,
+                ImageSrc = o.ImageSrc
+            }).FirstOrDefault();
+        }
+
+        public void CreateOrdinace(OrdinaceViewModel viewModel)
+        {
+            var ordinace = new Ordinace
+            {
+                Budova = viewModel.Budova,
+                Mistnost = viewModel.Mistnost,
+                DoktorID = viewModel.DoktorId
+            };
+
+            if (viewModel.Image != null)
+            {
+                string imageSrc = _fileUploadService.FileUpload(viewModel.Image, Path.Combine("img", "ordinace"));
+                ordinace.ImageSrc = imageSrc;
+            }
+
+            _context.Ordinace.Add(ordinace);
+            _context.SaveChanges();
+        }
+
+        public void UpdateOrdinace(OrdinaceViewModel viewModel)
+        {
+            var ordinace = _context.Ordinace.FirstOrDefault(o => o.Id == viewModel.OrdinaceId);
+            if (ordinace != null)
+            {
+                ordinace.Budova = viewModel.Budova;
+                ordinace.Mistnost = viewModel.Mistnost;
+                ordinace.DoktorID = viewModel.DoktorId;
+
+                if (viewModel.Image != null)
+                {
+                    string imageSrc = _fileUploadService.FileUpload(viewModel.Image, Path.Combine("img", "ordinace"));
+                    ordinace.ImageSrc = imageSrc;
+                }
+
+                _context.Ordinace.Update(ordinace);
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteOrdinace(int id)
+        {
+            var ordinace = _context.Ordinace.FirstOrDefault(o => o.Id == id);
+            if (ordinace != null)
+            {
+                _context.Ordinace.Remove(ordinace);
+                _context.SaveChanges();
+            }
         }
     }
     
---
+++ Nemocnice/Areas/Admin/Controllers/OrdinaceController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nemocnice.application.Abstraction;
+using Nemocnice.application.ViewModels;
+using Nemocnice.infrastructure.Identity.Enums;
+
+namespace Nemocnice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = nameof(Roles.Admin))]
+    public class OrdinaceController : Controller
+    {
+        private readonly IOrdinaceService _ordinaceService;
+
+        public OrdinaceController(IOrdinaceService ordinaceService)
+        {
+            _ordinaceService = ordinaceService;
+        }
+
+        public IActionResult Select()
+        {
+            var ordinace = _ordinaceService.GetOrdinace();
+            return View(ordinace);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new OrdinaceViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(OrdinaceViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _ordinaceService.CreateOrdinace(viewModel);
+                return RedirectToAction(nameof(Select));
+            }
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var ordinace = _ordinaceService.GetOrdinaceById(id);
+            if (ordinace == null)
+            {
+                return NotFound();
+            }
+            return View(ordinace);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(OrdinaceViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _ordinaceService.UpdateOrdinace(viewModel);
+                return RedirectToAction(nameof(Select));
+            }
+            return View(viewModel);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _ordinaceService.DeleteOrdinace(id);
+            return RedirectToAction(nameof(Select));
+        }
+    }
+}
